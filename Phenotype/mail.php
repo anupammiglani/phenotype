d758a43dead7b1e4bdafe4654347ea2e4accae75
anupammiglani <mprabagar80@gmail.com>
2019-10-14 18:09:33 +0530

    Project Images

--- Phenotype/mail.php
+++ Phenotype/mail.php
@@ -8,14 +8,14 @@ require './sendgrid-php/vendor/autoload.php';// If you're using Composer (recomm
 $email = new \SendGrid\Mail\Mail();
 $email->setFrom("test@example.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("test@example.com", "Example User");
+$email->addTo("dharmendrajadon@live.in, "Example User");
 $email->addContent(
     "text/plain", "and easy to do anywhere, even with PHP"
 );
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('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'));
+$sendgrid = new \SendGrid('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');
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
